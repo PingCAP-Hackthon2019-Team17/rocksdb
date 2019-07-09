c5d6f4f6b91b2ea71f812185a9b0ea6893c6997c
Yi Wu <yiwu@pingcap.com>
2019-07-09 18:22:17 +0000

    builder: not to split file
    Signed-off-by: Yi Wu <yiwu@pingcap.com>

--- db/builder.cc
+++ db/builder.cc
@@ -328,11 +328,13 @@ Status BuildPartTable(
           current_guard = Slice();
         }
 
-         // Split sst by guard when sst reach specified size.
+        // Split sst by guard when sst reach specified size.
+        /*
         uint64_t split_size = ioptions.level0_split_size > 0 ? ioptions.level0_split_size : 32*1024*1024;
         if (builder->FileSize() >= split_size) {
           break;
         }
+        */
       }
 
        builder->Add(key, value);
